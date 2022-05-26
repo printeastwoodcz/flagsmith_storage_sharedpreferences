5ae68ba00fb4606906baa5cfcc31ee4c765a2703
zoocityboy <zoocityboy@gmail.com>
2022-05-26 23:32:09 +0200

    bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+## 3.0.0-beta.1
+* bump version, support flutter >=3.0.0
 ## 2.1.0+1
 * update naming
 ## 2.1.0
--- lib/sharedpreferences_store.dart
+++ lib/sharedpreferences_store.dart
@@ -1,5 +1,5 @@
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flagsmith_core/flagsmith_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FlagsmithSharedPreferenceStore extends CoreStorage {
   SharedPreferences? _prefs;
